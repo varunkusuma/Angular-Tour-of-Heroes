38f2a0ba03803e0c9ee8ae47d446582e27bee7c7
varunkusuma <varunkusuma1664@gmail.com>
2019-05-01 17:37:41 +0530

    Prior to Http

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
+
+@Component({
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
+})
+export class HeroesComponent implements OnInit {
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+  }
+
+  heroes: Hero[];
+}
